dea4c79e210ebff23f0e0634bfcded934798cf2e
James Tsai <angel222634@gmail.com>
2023-05-08 15:52:52 +0800

    Update CI_demo app:create urls.py and insert index function in views.py

--- CI_demo/tests.py
+++ CI_demo/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestHomePageView(TestCase):
+    def test_reachable_home(self):
+        response = self.client.get('/CI_demo/home')
+        self.assertEqual(response.status_code,200)
